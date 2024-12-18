2b44323b443e7497a580576ce8b5cfba68ce9429
Trọng Khiêm <kit10012003@gmail.com>
2024-12-18 08:47:20 +0700

    Đổi lại y cũ

--- Client/src/User/pages/Search/SearchSongs.jsx
+++ Client/src/User/pages/Search/SearchSongs.jsx
@@ -1,37 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import ListSong from "../Album/listSong"; // Component hiển thị danh sách bài hát
 import { useDispatch, useSelector } from "react-redux";
 import * as actions from "../../Store/Action";
-import Loading from "../../components/Loaded-Spinner/Loading";
 
 const SearchSongs = () => {
   const { searchData } = useSelector((state) => state.music);
-  const [isLoading, setIsLoading] = useState(true);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    setIsLoading(true);
-    try {
-      dispatch(actions.getSearchSongs(searchData?.top?.id));
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsLoading(false);
-    }
+    dispatch(actions.getSearchSongs(searchData?.top?.id));
   }, [searchData]);
 
   return (
-    <>
-      {isLoading ? (
-        <div className="flex items-center justify-center h-screen -mt-32">
-          <Loading />
-        </div>
-      ) : (
-        <div className="px-[60px]">
-          <ListSong isHideTime={true} />
-        </div>
-      )}
-    </>
+    <div className="px-[60px]">
+      <ListSong isHideTime={true} />
+    </div>
   );
 };
 
